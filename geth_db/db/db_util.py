8cb00e6a9acef2640f25a98692e00b157aff9334
Matus Gallik <matusgallik008@gmail.com>
2021-02-24 11:37:48 +0100

    fix: ui

--- geth_db/db/db_util.py
+++ geth_db/db/db_util.py
@@ -1,4 +1,7 @@
-def getByGUID(GUID, db):
+from python_geth.node import Node
+
+
+def get_by_guid(GUID, db):
     amount_of_subjects = db.functions.get_studentSubjectAmount(GUID).call()
     subjects = {}
     for i in range(amount_of_subjects):
@@ -8,8 +11,38 @@ def getByGUID(GUID, db):
     return subjects
 
 
-def createOrUpdate(GUID, subject, grade, db, w3,account):
-    tx_hash = db.functions.add_grade(GUID, subject, grade).transact({'from':account})
+def create_or_update(GUID, subject, grade, db, w3, account):
+    tx_hash = db.functions.add_grade(GUID, subject, grade).transact({'from': account})
     tx_receipt = w3.eth.waitForTransactionReceipt(tx_hash)
 
     return tx_receipt
+
+
+def run_parent_node(datadir):
+    node = Node(datadir=datadir, port=30303,
+                rpcport=8000, name="Node01")
+    node.start_node()
+
+    account, password = node.get_first_account()
+    node.w3.geth.personal.unlock_account(account, password)
+    return node, account, password
+
+
+def run_child_node(datadir):
+    node = Node(datadir=datadir, genesis_file="/home/matus/Desktop/Uni/genesis.json")
+    enode = "enode://20ab04b6abe745b103aa2d366889b55d747b78edf7265cbd1dd17c83cd9428ddb26fa721e27d113f3c09e16931711f343c98535ddf28aae3d3e931744dfabcb8@127.0.0.1:30303?discport=0"
+
+    # Start node
+    node.start_node()
+    node.w3.geth.admin.add_peer(enode)
+
+    name = "UTC--2021-02-24T09-46-48.819455600Z--e5d1d5eac3b806f817c6abc5cf8cb2bb86502d8f"
+    key = "/home/matus/Desktop/Uni/{}".format(name)
+    account = "0xe5D1D5eAC3B806F817C6abc5cF8cB2Bb86502D8F"
+    password = "2e847a7039d88c8770cbfb62bb0e73e54f7c775f1434552dde7a4ec66692ed48"
+    node.add_foreign_account(name=name, key=key, password=password)
+    r = node.w3.geth.personal.unlock_account(account, password)
+    print("PEER COUNT: {}".format(node.w3.net.peerCount))
+    print("UNLOCK ACCOUNT: {}".format(r))
+
+    return node, account, password
--- geth_db/run_geth_db.py
+++ geth_db/run_geth_db.py
@@ -5,10 +5,10 @@ import time
 from flask import Flask, request
 from flask_cors import CORS
 from python_geth.contract_interface import ContractInterface
-from python_geth.node import Node
+
 from web3 import Web3
 
-from db.db_util import getByGUID, createOrUpdate
+from db.db_util import get_by_guid, create_or_update, run_parent_node, run_child_node
 
 app = Flask(__name__)
 CORS(app)
@@ -21,7 +21,7 @@ def get():
 
     node.w3.geth.personal.unlock_account(account, password)
 
-    res = getByGUID(guid, guid_db_contract)
+    res = get_by_guid(guid, guid_db_contract)
     res = json.loads(Web3.toJSON(res))
     res.update({'time': (time.time() - start_time)})
     return json.dumps(res)
@@ -38,7 +38,7 @@ def create():
     print(account)
     print(password)
 
-    res = createOrUpdate(guid, subject, grade, guid_db_contract, node.w3, account)
+    res = create_or_update(guid, subject, grade, guid_db_contract, node.w3, account)
     res = json.loads(Web3.toJSON(res))
     return json.dumps({'tx': res, 'time': (time.time() - start_time)})
 
@@ -52,53 +52,28 @@ def update():
 
     node.w3.geth.personal.unlock_account(account, password)
 
-    res = createOrUpdate(guid, subject, grade, guid_db_contract, node.w3, account)
+    res = create_or_update(guid, subject, grade, guid_db_contract, node.w3, account)
     res = json.loads(Web3.toJSON(res))
     return json.dumps({'tx': res, 'time': (time.time() - start_time)})
 
 
-def run_parent_node():
-    node = Node(datadir=datadir, port=30303,
-                rpcport=8000, name="Node01")
-    node.start_node()
-
-    account, password = node.get_first_account()
-    node.w3.geth.personal.unlock_account(account, password)
-    return node, account, password
-
-
-def run_child_node():
-    node = Node(datadir=datadir, genesis_file="/home/matus/Desktop/Uni/genesis.json")
-    enode = "enode://20ab04b6abe745b103aa2d366889b55d747b78edf7265cbd1dd17c83cd9428ddb26fa721e27d113f3c09e16931711f343c98535ddf28aae3d3e931744dfabcb8@127.0.0.1:30303?discport=0"
-
-    # Start node
-    node.start_node()
-    node.w3.geth.admin.add_peer(enode)
-
-    name = "UTC--2021-02-24T09-46-48.819455600Z--e5d1d5eac3b806f817c6abc5cf8cb2bb86502d8f"
-    key = "/home/matus/Desktop/Uni/{}".format(name)
-    account = "0xe5D1D5eAC3B806F817C6abc5cF8cB2Bb86502D8F"
-    password = "2e847a7039d88c8770cbfb62bb0e73e54f7c775f1434552dde7a4ec66692ed48"
-    node.add_foreign_account(name=name, key=key, password=password)
-    r = node.w3.geth.personal.unlock_account(account, password)
-    print("PEER COUNT: {}".format(node.w3.net.peerCount))
-    print("UNLOCK ACCOUNT: {}".format(r))
-
-    return node, account, password
-
-
 if __name__ == "__main__":
-    num = input("Is this the 1 node or 2 node?")
+    num = input("If this is single node press 3\n If this is 1st node of double press 1, otherwise press 2")
 
-    if num == "1":
+    if os.name == 'nt':
         datadir = "C:\\Users\\matus\\Desktop\\Uni\\node01"
-        os.system("rm -rf \"{}\"".format(datadir))  # debug purposes
-        node, account, password = run_parent_node()
     else:
         datadir = "/home/matus/Desktop/node01"
-        os.system("rm -rf \"{}\"".format(datadir))  # debug purposes
-        node, account, password = run_child_node()
+
+    os.system("rm -rf \"{}\"".format(datadir))  # debug purposes
+
+    if num == "1" or num == "3":
+        node, account, password = run_parent_node(datadir)
+    else:
+        node, account, password = run_child_node(datadir)
+
     node.configure_truffle()
+
     input("To start mining hit enter")
     node.w3.geth.miner.start(1)
 
@@ -106,7 +81,7 @@ if __name__ == "__main__":
     CI = ContractInterface(w3=node.w3,
                            datadir=datadir)
 
-    if num == "2":
+    if num == "2" or num == '3':
         print("deploying")
         try:
             guid_db_contract = CI.deploy_contract(
